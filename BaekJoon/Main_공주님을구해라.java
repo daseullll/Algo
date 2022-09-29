8138b3673ab60924b23df247a20344669bc5032b
안다슬 <dew4325@naver.com>
2022-09-29 23:17:33 +0900

    S_Main 17836

--- BaekJoon/Main_공주님을구해라.java
+++ BaekJoon/Main_공주님을구해라.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main_17836 {
+public class Main_공주님을구해라 {
     static int N, M, T, result;
     static int[][] delta = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
     static int[][] map;
@@ -32,7 +32,6 @@ public class Main_17836 {
         result = Integer.MAX_VALUE;
 
         map = new int[N][M];
-        queue = new LinkedList<Node>();
         check = new boolean[N][M];
 
         for (int i = 0; i < N; i++) {
@@ -46,11 +45,8 @@ public class Main_17836 {
             }
         }
 
-        queue.add(new Node(0, 0, 0));
+        bfs();
 
-        while (!queue.isEmpty()) {
-            bfs(queue.poll());
-        }
         if (result == Integer.MAX_VALUE)
             sb.append("Fail");
         else
@@ -59,33 +55,50 @@ public class Main_17836 {
 
     }
 
-    public static void bfs(Node current) {
-        int r = current.r;
-        int c = current.c;
-        int dis = current.dis;
+    public static void bfs() {
 
-        check[r][c] = true;
+        queue = new LinkedList<Node>();
+        queue.add(new Node(0, 0, 0));
+        check[0][0] = true;
 
-        if (dis > T)
-            return;
-        if (r == gramX && c == gramY) {
-            dis = dis + ((N - 1) - r) + ((M - 1) - c);
-            if (dis <= T)
-                result = Math.min(result, dis);
-            return;
-        }
-        if (r == N - 1 && c == M - 1) {
-            result = Math.min(result, dis);
-            return;
-        }
+        Loop: while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            int r = current.r;
+            int c = current.c;
+            int dis = current.dis;
 
-        for (int dir = 0; dir < 4; dir++) {
-            int rn = r + delta[dir][0];
-            int cn = c + delta[dir][1];
-            if (rn < 0 || cn < 0 || rn >= N || cn >= M || check[rn][cn] == true || map[rn][cn] == 1) {
+            if (dis > T)
                 continue;
+
+            if (r == N - 1 && c == M - 1) {
+                result = Math.min(result, dis);
+                break;
             }
-            queue.add(new Node(rn, cn, dis + 1));
+
+            for (int dir = 0; dir < 4; dir++) {
+                int rn = r + delta[dir][0];
+                int cn = c + delta[dir][1];
+                if (rn < 0 || cn < 0 || rn >= N || cn >= M || check[rn][cn] == true || map[rn][cn] == 1) {
+                    continue;
+                }
+                if (map[rn][cn] == 2) {
+                    queue.add(new Node(rn, cn, dis + 1));
+                }
+
+                // 그람을 발견했을때
+                if (rn == gramX && cn == gramY) {
+                    dis = dis + ((N - 1) - rn) + ((M - 1) - cn) + 1;
+                    if (dis <= T) {
+                        result = Math.min(result, dis);
+                        continue Loop;
+                    }
+                } else {
+                    check[rn][cn] = true;
+                }
+
+                queue.add(new Node(rn, cn, dis + 1));
+            }
+
         }
     }
 
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-| 날짜    | 문제           | 기타        | 풀이               |
-|-------|--------------| ----------- | ------------------ |
-| 09.07 | 신고결과받기       | Programmers | :heavy_check_mark: |
-| 09.08 | 같은숫자는싫어      | Programmers | :heavy_check_mark: |
-| 09.16 | 캐시           | Programmers | :heavy_check_mark: |
-| 09.18 | 비밀지도         | Programmers | :heavy_check_mark: |
-| 09.20 | 숫자 문자열과 영단어  | Programmers | :heavy_check_mark: |
+| 날짜  | 문제                    | 기타        | 풀이               |
+| ----- | ----------------------- | ----------- | ------------------ |
+| 09.07 | 신고결과받기            | Programmers | :heavy_check_mark: |
+| 09.08 | 같은숫자는싫어          | Programmers | :heavy_check_mark: |
+| 09.16 | 캐시                    | Programmers | :heavy_check_mark: |
+| 09.18 | 비밀지도                | Programmers | :heavy_check_mark: |
+| 09.20 | 숫자 문자열과 영단어    | Programmers | :heavy_check_mark: |
 | 09.22 | k진수에서소수개수구하기 | Programmers | :heavy_check_mark: |
+| 09.28 | 공주님을구해라(17836)   | BaekJoon    | :heavy_check_mark: |
