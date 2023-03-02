b1b69ca06814458d65260a2a52cf4e38735d3c6c
안다슬 <dew4325@naver.com>
2023-03-02 23:45:38 +0900

    Solution_조이스틱

--- Programmers/Solution_조이스틱.java
+++ Programmers/Solution_조이스틱.java
@@ -7,7 +7,7 @@ public class Solution_조이스틱 {
         int length = name.length();
 
         int index; // 다음 값들을 확인할때 사용
-        int move = length - 1; // 좌우 움직임 수를 체크
+        int move = length - 1; // 오르쪽으로 쭉 간 횟수
 
         for (int i = 0; i < name.length(); i++) {
             answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
@@ -19,7 +19,9 @@ public class Solution_조이스틱 {
             }
 
             // 순서대로 가는 것과 뒤로 돌아가는 것 중 이동수가 적은 것을 선택
+            // 오른쪽으로 갔다가 다시 왼쪽으로 가는 경우
             move = Math.min(move, i * 2 + length - index);
+            // 왼쪽으로 갔다가 다시 오른쪽으로 가는 경우
             move = Math.min(move, (length - index) * 2 + i);
         }
 
